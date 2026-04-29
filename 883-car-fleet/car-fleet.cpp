50918017096c4cd3859527d85c30d3b2c3f16921
prince9661 <149985850+prince9661@users.noreply.github.com>
2026-04-29 11:04:18 +0530

    Time: 39 ms (75.31%) | Memory: 104 MB (79.24%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -6,15 +6,22 @@ public:
             car.push_back({position[i],speed[i]});
         }
         sort(car.begin(),car.end(),greater<>());
-        stack<double>st;
-        double d=(1.0*(target-car[0].first))/(1.0*car[0].second);
-        st.push(d);
-        for(int i=1;i<speed.size();i++){
+        // stack<double>st;
+        // double d=(1.0*(target-car[0].first))/(1.0*car[0].second);
+        // st.push(d);
+        int count=0;
+        double nettime=0.0;
+        for(int i=0;i<speed.size();i++){
             double time=(1.0*(target-car[i].first))/(1.0*car[i].second);
-            if(st.top()<time){
-                st.push(time);
+            // if(st.top()<time){
+            //     st.push(time);
+            // }
+            if(time>nettime){
+                count++;
+                nettime=time;
             }
         }
-        return st.size();
+        // return st.size();
+        return count;
     }
 };
