bba06f042047e3baa51305e6540480d25fb1032d
prince9661 <149985850+prince9661@users.noreply.github.com>
2026-04-29 11:02:20 +0530

    Time: 45 ms (57.08%) | Memory: 105.9 MB (58.13%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>>car;
+        for(int i=0;i<speed.size();i++){
+            car.push_back({position[i],speed[i]});
+        }
+        sort(car.begin(),car.end(),greater<>());
+        stack<double>st;
+        double d=(1.0*(target-car[0].first))/(1.0*car[0].second);
+        st.push(d);
+        for(int i=1;i<speed.size();i++){
+            double time=(1.0*(target-car[i].first))/(1.0*car[i].second);
+            if(st.top()<time){
+                st.push(time);
+            }
+        }
+        return st.size();
+    }
+};
