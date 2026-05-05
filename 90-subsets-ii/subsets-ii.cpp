f25f7755441103c99651cb1460c693797020aab6
prince9661 <149985850+prince9661@users.noreply.github.com>
2026-05-05 18:59:51 +0530

    Time: 3 ms (31.40%) | Memory: 14.9 MB (10.23%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void solve(int i,vector<int>&res,vector<int>&nums){
-        ans.push_back(res);
+    // void solve(int i,vector<int>&res,vector<int>&nums){
+    //     ans.push_back(res);
         
-        for(int j=i;j<nums.size();j++){
-            if(j>i&&nums[j]==nums[j-1])continue;
-            res.push_back(nums[j]);
-            solve(j+1,res,nums);
-            res.pop_back();
-        }
+    //     for(int j=i;j<nums.size();j++){
+    //         if(j>i&&nums[j]==nums[j-1])continue;
+    //         res.push_back(nums[j]);
+    //         solve(j+1,res,nums);
+    //         res.pop_back();
+    //     }
         
-    }
-    // void solve(int i,vector<int>&res,vector<int>&nums,unordered_set<int>&st){
-    //     if(i==nums.size()){
-    //         ans.push_back(res);
-    //         return;
-    //     }
-    //     if(!st.count(nums[i])){
-    //         solve(nums)
-    //     }
     // }
+    void solve(int i,vector<int>&res,vector<int>&nums,unordered_set<int>&st){
+        if(i==nums.size()){
+            ans.push_back(res);
+            return;
+        }
+        if(!st.count(nums[i])){
+            solve(i+1,res,nums,st);
+
+        }
+        st.insert(nums[i]);
+        res.push_back(nums[i]);
+        solve(i+1,res,nums,st);
+        res.pop_back();
+        st.erase(nums[i]);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int>res;
-        solve(0,res,nums);
+        unordered_set<int>st;
+        solve(0,res,nums,st);
         return ans;
     }
 };
